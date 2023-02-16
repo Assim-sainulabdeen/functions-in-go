782fec2f21545f04741e8f03ca7ef11130c6d2c9
Assim <assim.s@techversantinfo.com>
2023-02-16 13:30:51 +0530

    initial commit

---
+++ anonymousfn.go
@@ -0,0 +1,16 @@
+    // Go program to illustrate how
+    // to create an anonymous function
+    package main
+
+    import "fmt"
+
+    func main() {
+        
+        // Anonymous function
+    func(){
+
+        fmt.Println("Welcome! to GeeksforGeeks")
+    }()
+        
+    }
+
